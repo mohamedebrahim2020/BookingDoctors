3f930cc857906098c86df478884f086b85d99c30
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2021-01-14 11:28:32 +0200

    feature:feature_tests_for_update_admin

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -188,5 +188,100 @@ class AdminTest extends TestCase
         $response->assertExactJson(["only super admin can delete admin and not be deleted"]);
         $response->assertForbidden();
     }
+
+    /** @test */
+    public function superadmin_sucessfully_update_admin()
+    {
+        $superAdmin = Admin::where('is_super', 1)->first();
+        Passport::actingAs($superAdmin, ['*'], 'admin');
+        $admin = Admin::factory()->create();
+        $data = [
+            'name' => 'mohamed',
+            'email' => 'adam@gmail.com',
+            'phone' => '01225000539',
+            'permissions' => [1, 2, 3],
+        ];
+        $response = $this->putJson('/api/admins/' . $admin->id, $data);
+        $response->assertOk();
+    }
+
+    /** @test */
+    public function admin_not_authorized_to_update()
+    {
+        $admin = Admin::factory()->create();
+        Passport::actingAs($admin, ['*'], 'admin');
+        $data = [
+            'name' => 'mohamed',
+            'email' => 'adam@gmail.com',
+            'phone' => '01225000539',
+            'permissions' => [1, 2, 3],
+        ];
+        $response = $this->putJson('/api/admins/' . $admin->id, $data);
+        $response->assertExactJson(["You do not authorized to update admin"]);
+        $response->assertForbidden();
+    }
+
+    /** @test */
+    public function superadmin_fail_to_update_admin_with_already_existing_email()
+    {
+        $superAdmin = Admin::where('is_super', 1)->first();
+        $firstAdmin = Admin::factory()->create();
+        $secondAdmin = Admin::factory()->create();
+        Passport::actingAs($superAdmin, ['*'], 'admin');
+        $data = [
+            'name' => 'mohamed',
+            'email' => $secondAdmin->email,
+            'phone' => '01225000539',
+            'permissions' => [1, 2, 3],
+        ];
+        $response = $this->putJson('/api/admins/' . $firstAdmin->id, $data);
+        $response->assertJsonValidationErrors('email');
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function superadmin_fail_to_update_admin_with_not_existing_permission()
+    {
+        $superAdmin = Admin::where('is_super', 1)->first();
+        $firstAdmin = Admin::factory()->create();
+        Passport::actingAs($superAdmin, ['*'], 'admin');
+        $data = [
+            'name' => 'mohamed',
+            'email' => $firstAdmin->email,
+            'phone' => '01225000539',
+            'permissions' => [1, 2, 4],
+        ];
+        $response = $this->putJson('/api/admins/' . $firstAdmin->id, $data);
+        $response->assertJsonValidationErrors('permissions.2');
+        $response->assertExactJson([
+            "message" =>  "The given data was invalid.",
+            "errors" => [
+                "permissions.2" => [
+                    "The selected permissions.2 is invalid."
+                ],
+            ]
+        ]);
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function superadmin_fail_to_update_not_found_admin_model()
+    {
+        $superAdmin = Admin::where('is_super', 1)->first();
+        Passport::actingAs($superAdmin, ['*'], 'admin');
+        $data = [
+            'name' => 'mohamed',
+            'email' => 'm@gmail.com',
+            'phone' => '01225000539',
+            'permissions' => [1, 2, 3],
+        ];
+        $response = $this->putJson('/api/admins/2', $data);
+        $response->assertExactJson([
+            "error" => "no model  admin with this identifier",
+        ]);
+        $response->assertNotFound();
+    }
+
+    
     
 }
