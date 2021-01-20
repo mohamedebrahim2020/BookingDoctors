6abaa6bee7a989c24dc5a9eb8cc8a6feb63fcb78
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2021-01-20 05:55:24 +0200

    fix:make_photo_name_as_path_to_local_storage

--- app/Services/DoctorService.php
+++ app/Services/DoctorService.php
@@ -4,6 +4,8 @@ namespace App\Services;
 
 use App\Repositories\DoctorRepository;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Str;
 
 class DoctorService extends BaseService
 {
@@ -15,14 +17,21 @@ class DoctorService extends BaseService
     public function store($data)
     {
         
+        
+        $data['photo'] = $this->addFileToPublic($data['photo'],'/photo');
+        $data['degree_copy'] = $this->addFileToPublic($data['degree_copy'],'/degree_copy');
         $doctor = $this->repository->store($data);
-        $this->addFileToPublic($data['photo'],'photo');
-        $this->addFileToPublic($data['degree_copy'],'degree_copy');
         return $doctor;
     }
 
     public function addFileToPublic($file, $folder)
     {
-        Storage::put($folder, $file);
+        $fileName   = time() . '.' . $file->getClientOriginalExtension();
+        $img = Image::make($file->getRealPath());
+        $randomString = Str::random(15);
+        $fileNametostore = $folder . '/'. $randomString . $fileName;
+        Storage::put($fileNametostore, $img);
+        return $fileNametostore;
+        
     }
 }    
