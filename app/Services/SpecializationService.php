c284411bb547ff23abb48ff64ec975a0bbb44916
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2021-02-09 11:46:23 +0200

    feature_cache_specialization

--- app/Services/SpecializationService.php
+++ app/Services/SpecializationService.php
@@ -3,11 +3,20 @@
 namespace App\Services;
 
 use App\Repositories\SpecializationRepository;
+use Illuminate\Support\Facades\Cache;
 
 class SpecializationService extends BaseService
 {
     public function __construct(SpecializationRepository $repository)
     {
         $this->repository = $repository;
+    }
+
+    public function index()
+    {
+        $specializations = Cache::remember('specializations', 33600 , function () {
+            return $this->repository->all();
+        });
+        return $specializations;
     }
 }
