fc394842886976dc478f1ba07384e91c12512d2a
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2021-02-14 12:27:05 +0200

    add_observer_functions_created_and_deleted

--- app/Observers/DoctorObserver.php
+++ app/Observers/DoctorObserver.php
@@ -8,12 +8,40 @@ use Illuminate\Support\Facades\Cache;
 class DoctorObserver
 {
     /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(Doctor $doctor)
+    {
+        $key = 'doctor_' . $doctor->id;
+        $doctor = Cache::remember($key, 33600, function () use($doctor) {
+            return $doctor;
+        });
+    }
+
+    /**
      * Handle the Doctor "updated" event.
      *
      * @param  \App\Models\Doctor  $doctor
      * @return void
      */
     public function updated(Doctor $doctor)
+    {
+        $key = 'doctor_'. $doctor->id;
+        if (Cache::has($key)) {
+            Cache::forget($key);
+        }
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(Doctor $doctor)
     {
         $key = 'doctor_'. $doctor->id;
         if (Cache::has($key)) {
