b682cedc5f0e4d3928969f17365b2936a99b9fbc
himaaaintake40 <mohamedebrahimabbas@gmail.com>
2021-02-28 11:59:02 +0200

    feature_doctor_respond_a_review

--- tests/Feature/DoctorRespondToReviewTest.php
+++ tests/Feature/DoctorRespondToReviewTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Enums\AppointmentStatus;
+use App\Enums\PlatformType;
 use App\Jobs\PushNotification;
 use App\Models\Appointment;
 use App\Models\Doctor;
@@ -12,6 +13,8 @@ use Carbon\Carbon;
 use Database\Seeders\DoctorSpecializationsSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Queue;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 
@@ -26,9 +29,8 @@ class DoctorRespondToReviewTest extends TestCase
     }
 
     /** @test */
-    public function Admin_successfully_delete_review()
+    public function doctor_successfully_respond_review()
     {
-        $this->withoutExceptionHandling();
         $doctor = Doctor::factory()->create(["activated_at" => Carbon::now()]);
         $patient = Patient::factory()->create(["verified_at" => Carbon::now()]);
         $doctor->workingDays()->create(
@@ -46,10 +48,48 @@ class DoctorRespondToReviewTest extends TestCase
         $data = [
             'respond' => 'thanks'
         ];
+        $patient->firebaseTokens()->updateOrCreate(
+            ['platform' => PlatformType::WEB],
+            ['token' => 'jjj'],
+        );
+        Queue::fake();
         Bus::fake();
         $response = $this->putJson(route('reviews.update', ['review' => $review->id]), $data, ["Accept" => "application/json"]);
         $response->assertOk();
         Bus::assertDispatched(PushNotification::class);
         Bus::assertDispatchedAfterResponse(PushNotification::class);
+    }
+
+    
+    /** @test */
+    public function doctor_failed_to_respond_not_found_review()
+    {
+        $doctor = Doctor::factory()->create(["activated_at" => Carbon::now()]);
+        $patient = Patient::factory()->create(["verified_at" => Carbon::now()]);
+        $doctor->workingDays()->create(
+            [
+                "day"=> "1",
+                "is_all_day"=> "1"
+            ]
+        );
+        Passport::actingAs($doctor, ['*'], 'doctor');
+        $review = Review::factory()->for(Appointment::factory()->state([
+            'doctor_id' => $doctor->id,
+            'patient_id' => $patient->id,
+            'status' => AppointmentStatus::COMPLETED,
+        ]))->create();
+        $data = [
+            'respond' => 'thanks'
+        ];
+        $patient->firebaseTokens()->updateOrCreate(
+            ['platform' => PlatformType::WEB],
+            ['token' => 'jjj'],
+        );
+        Queue::fake();
+        Bus::fake();
+        $response = $this->putJson(route('reviews.update', ['review' => 2]), $data, ["Accept" => "application/json"]);
+        $response->assertNotFound();
+        Bus::assertNotDispatched(PushNotification::class);
+        Bus::assertNotDispatchedAfterResponse(PushNotification::class);
     }
 }
